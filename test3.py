a4a9e273c7b47cb36d72321cd334ab68f881e807
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-07-02 22:11:05 -0400

    Update test3.py

--- test3.py
+++ test3.py
@@ -58,8 +58,11 @@ coords_fem, V_vals_fem, T_vals_fem = run_fem(
     coords=graph.pos.cpu().numpy(),
     sigma_vals = sigma.cpu().numpy().flatten(),
     k_vals     = kappa.cpu().numpy().flatten(),
+    lb         = (-2.0, -2.0),   # example for testing on [-1,1]^2
+    ru         = ( 2.0,  2.0),
+    V_D        = 1.0,            # your Dirichlet voltage top face
+    T_D        = 273.0           # your Dirichlet temperature on all faces
 )
-
 # ensure ordering of coords matches GNN's graph.pos; if not, you'd need to reorder.
 
 # 3) Compute and print relative L2 errors
