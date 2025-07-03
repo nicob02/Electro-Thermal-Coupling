e1327025de275bec78cb36c122978345f60f04e4
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-07-02 22:11:39 -0400

    Update test.py

--- test.py
+++ test.py
@@ -58,6 +58,10 @@ coords_fem, V_vals_fem, T_vals_fem = run_fem(
     coords=graph.pos.cpu().numpy(),
     sigma_vals = sigma.cpu().numpy().flatten(),
     k_vals     = kappa.cpu().numpy().flatten(),
+    lb         = (-0.5, -0.5),   # example for testing on [-1,1]^2
+    ru         = ( 0.5,  0.5),
+    V_D        = 1.0,            # your Dirichlet voltage top face
+    T_D        = 273.0           # your Dirichlet temperature on all faces
 )
 
 # ensure ordering of coords matches GNN's graph.pos; if not, you'd need to reorder.
