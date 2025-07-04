4c650dc72128611f127e6cbdd07c0fe141b33be6
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-07-04 04:01:12 -0400

    Update train.py

--- train.py
+++ train.py
@@ -77,9 +77,9 @@ setattr(train_config, 'graph_modify', func_main.graph_modify)
 setattr(train_config, 'graph', graph)
 setattr(train_config, 'model', model)
 setattr(train_config, 'optimizer', optimizer)
-setattr(train_config, 'epchoes', 4000)
+setattr(train_config, 'epchoes', 40)
 # **when** to switch domain:
-setattr(train_config, 'change_domain_epoch', 3900)
+setattr(train_config, 'change_domain_epoch', 39)
 setattr(train_config, 'domain_step', 100)
 # **to what** new domain
 setattr(train_config, 'new_lb', (-1.5, -1.5))
